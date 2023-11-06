012a9acd971e2ea9aeb3c116c44da8d138b23757
Jordan Jones <144819530+morelikesinx@users.noreply.github.com>
2023-11-06 14:37:15 +0000

    added and done ClassFunctReview; clone activity

---
+++ sourceCode/RJ_program.py
@@ -0,0 +1,8 @@
+def fun():
+    sports = input('do you like basketball')
+    if sports == 'Yes': True
+    print('NIce a like being that court')
+    if sports == 'No': False
+    print('Ok well you should play it sometime, its really fun')
+
+fun()   
